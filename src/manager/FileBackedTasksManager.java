1adca4343cbbed51ed3ea165216d31dbf7375072
Daniil <rusbear060@gmail.com>
2023-01-13 15:27:26 +0300

    add CSVTaskFormat class and fix some style issues

--- src/manager/FileBackedTasksManager.java
+++ src/manager/FileBackedTasksManager.java
@@ -2,24 +2,16 @@ package manager;
 
 import manager.exception.ManagerSaveException;
 import task.Epic;
-import task.Status;
 import task.Subtask;
 import task.Task;
 
 import java.io.*;
 import java.nio.file.Files;
 import java.nio.file.Path;
-import java.util.Map.Entry;
-
-import java.util.ArrayList;
-
-//import static manager.CSVTaskFormat.*;
 
 public class FileBackedTasksManager extends InMemoryTaskManager implements TaskManager {
 
     public static void main(String[] args) {
-//        testSaving();
-        testLoading();
     }
 
     static void testSaving() {
@@ -154,66 +146,6 @@ public class FileBackedTasksManager extends InMemoryTaskManager implements TaskM
         }
 
     }
-
-//    private String getAllTasks() {
-//        String result = "";
-//
-//        for (Task task : getTasks()) {
-//            result += task.toString() + "\n";
-//        }
-//
-//        for (Task task : getSubtasks()) {
-//            result += task.toString() + "\n";
-//        }
-//
-//        for (Task task : getEpics()) {
-//            result += task.toString() + "\n";
-//        }
-//
-//        return result;
-//    }
-
-//    static private String historyToString(HistoryManager historyManager) {
-//        String result = "";
-//
-//        for (Task task : historyManager.getHistory()) {
-//            result += task.getUid() + ",";
-//        }
-//
-//        if (result.length() > 0) result = result.substring(0, result.length() - 1);
-//
-//        return result;
-//    }
-
-//    private Task taskFromString(String taskString) {
-//        String[] taskStringArr = taskString.split(",");
-//
-//        switch (TaskType.valueOf(taskStringArr[1])) {
-//            case TASK:
-//                return new Task(Integer.parseInt(taskStringArr[0]), TaskType.valueOf(taskStringArr[1]),
-//                        taskStringArr[2], Status.valueOf(taskStringArr[3]), taskStringArr[4]);
-//            case SUBTASK:
-//                return new Subtask(Integer.parseInt(taskStringArr[0]), TaskType.valueOf(taskStringArr[1]),
-//                        taskStringArr[2], Status.valueOf(taskStringArr[3]), taskStringArr[4],
-//                        Integer.parseInt(taskStringArr[5]));
-//            case EPIC:
-//                return new Epic(Integer.parseInt(taskStringArr[0]), TaskType.valueOf(taskStringArr[1]),
-//                        taskStringArr[2], Status.valueOf(taskStringArr[3]), taskStringArr[4]);
-//        }
-//
-//        return null;
-//    }
-
-//    static ArrayList<Integer> historyFromString(String value) {
-//        String[] valueArr = value.split(",");
-//        ArrayList<Integer> result = new ArrayList<>();
-//        for (String str : valueArr) {
-//            result.add(Integer.parseInt(str));
-//        }
-//
-//        return result;
-//    }
-
 
     static FileBackedTasksManager loadFromFile(File file) {
         FileBackedTasksManager taskManager = new FileBackedTasksManager();
